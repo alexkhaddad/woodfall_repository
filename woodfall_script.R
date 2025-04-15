705fd38f709d8c14c267dcaeae2790b02a6889c6
alexkhaddad <alexander.haddad@students.mq.edu.au>
2025-04-15 19:24:01 +1000

    update R script

--- woodfall_script.R
+++ woodfall_script.R
@@ -11,7 +11,12 @@ woodfall.matrix <- woodfall[,-(1:8)]
 woodfall.matrix[is.na(woodfall.matrix)]=0 #looks through the matrix to check if NA
 rownames(woodfall.matrix) = woodfall$Log
 plot(hclust(dist(woodfall.matrix))) #2 distinct cluster with one outlier
+jpeg(filename="woodfall_hca.jpg", height = 480, width = 480) #turns plots into jpegs
+par(cex=1)
+par(mfrow=c(2,1)) #multiple plots
 plot(hclust(dist(t(woodfall.matrix))))
+plot(hclust(dist(woodfall.matrix)))
+dev.off()
 
 plot(hclust(vegdist(woodfall.matrix))) #euclidean is informed by absolute abundance rather than relative     
 
@@ -29,3 +34,11 @@ plot(cmdscale(vegdist(woodfall.matrix)), cex=0)
 text(cmdscale(vegdist(woodfall.matrix)), labels=rownames(woodfall.matrix))
 
 woodfall[woodfall$Log %in% c("3","7","10","19"),]
+
+#testing Xylophaga influence
+cmdscale(vegdist(t(woodfall.matrix)))
+k <- cmdscale(vegdist(t(woodfall.matrix)))
+plot(k)
+points(k[1:3,], pch=19, col="brown") #xylophaga does not have much influence
+which(k[,1]>0.4) #polychaetes 
+which(k[,2]>0.4) #gastropods
