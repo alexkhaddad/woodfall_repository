33391ed70dc4a9671b6eeffd1ce418af1bcbe327
alexkhaddad <alexander.haddad@students.mq.edu.au>
2025-05-19 20:47:35 +1000

    Adding glmm code and updating fisher's alpha

--- woodfall_script.R
+++ woodfall_script.R
@@ -18,14 +18,6 @@ plot(hclust(dist(t(woodfall.matrix))))
 plot(hclust(dist(woodfall.matrix)))
 dev.off()
 
-#fishers alpha
-alpha <- array()
-for (i in 1:ncol(woodfall.matrix)) {
-  n <- woodfall.matrix[,i]
-  alpha[i] <- fisher.alpha(n[n > 10])
-}
-alpha[alpha > 10] <- NA
-alpha <-
 
 plot(hclust(vegdist(woodfall.matrix))) #euclidean is informed by absolute abundance rather than relative     
 
@@ -51,3 +43,31 @@ plot(k)
 points(k[1:3,], pch=19, col="brown") #xylophaga does not have much influence
 which(k[,1]>0.4) #polychaetes 
 which(k[,2]>0.4) #gastropods
+
+
+#fishers alpha
+library(vegan)
+alpha <- array()
+for (i in 1:ncol(woodfall.matrix)) {
+  n <- woodfall.matrix[,i]
+  alpha[i] <- fisher.alpha(n[n > 10])
+}
+alpha[alpha > 10] <- NA
+alpha <- log(alpha)
+
+logs_data <- woodfall[,1:6]
+succession <- as.numeric(logs_data[,5])
+weight <- unlist(logs_data[,4])
+
+
+#generalized linear mixed model
+install.packages("lmerTest")
+library(lmerTest)
+install.packages("MuMIn")
+library(MuMIn)
+
+glmm <- lmer(alpha ~ )
+summary(glmm)
+ranef(glmm)
+rand(glmm)
+r.squaredGLMM(glmm)
